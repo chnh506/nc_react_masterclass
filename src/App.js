a6fc20caa04dca06484c9940308d269d00186e28
Chan <heochan01@korea.ac.kr>
2023-06-11 06:00:22 +0000

    styled-components : 'as' props & 'attrs()' function

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: blue;
+  border: 0;
+  padding: 10px;
+  border-radius: 15px;
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50%;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: whitesmoke;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+    <Father as="header">
+      <Btn>Log In</Btn>
+      <Btn as="a" href="https://www.notion.so/" target="_blank">
+        Go To Notion
+      </Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
