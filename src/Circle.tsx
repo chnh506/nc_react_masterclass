e8a70cb5d0c33d7513042fc6736302e9dad4b11c
Chan <heochan01@korea.ac.kr>
2023-06-26 08:01:48 +0000

    Typescript - optional props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 // interface는 JUST... "object의 모양을 설명하는 역할"이다!
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 // styled-component에서 Typescript를 활용하려면, 아래처럼 사용해 주면 OK
@@ -11,14 +12,21 @@ const Container = styled.div<ContainerProps>`
   height: 200px;
   border-radius: 50%;
   background-color: ${(props) => props.bgColor};
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = "deafult text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? "blue"}>
+      {text}
+    </Container>
+  );
 }
 
 export default Circle;
