96449739d41d5e96f6a1bab10720d0ac424dcfe7
Chan <heochan01@korea.ac.kr>
2023-07-10 10:45:53 +0000

    Add homepage

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,7 +5,7 @@ import Coins from "./routes/Coins";
 // Switch : 한 번에 하나의 Route를 렌더링할 수 있는 방법.
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/:coinId">
           <Coin />
